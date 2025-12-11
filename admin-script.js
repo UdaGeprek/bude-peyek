2eca357d5c8655eed68385e41f62ccd7b4c7a1db
UdaGeprek <rizkyfachrudin18@gmail.com>
2025-12-11 22:02:05 +0700

    Update admin-script.js

--- admin-script.js
+++ admin-script.js
@@ -10,7 +10,6 @@ if (!window.supabaseClient) {
     alert('Supabase belum terhubung! Pastikan supabase-config.js sudah di-load.');
 }
 
-supabase = window.supabaseClient;
 
 // State variables
 let products = [];
